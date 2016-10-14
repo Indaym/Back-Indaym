42f67744fc45d70905b3b300dde61f95ccbf2ef8
djavrell <kevin.prouteau@epitech.eu>
2016-10-14 14:35:40 +0800

    create config object

---
+++ config/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by djavrell on 14/10/16.
+ */
+
+module.exports = {
+    uri: 'mongodb://utxy1eefdmxlzii:Vlgt4N23pK2frbKkbyCT@bttd8eunqn0wbrb-mongodb.services.clever-cloud.com:27017/bttd8eunqn0wbrb'
+};
