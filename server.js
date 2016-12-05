c37eb3587801592587bc76f1d5ce644dcc064344
djavrell <kevin.prouteau@epitech.eu>
2016-12-05 16:52:28 +0800

    waterline is working
    * add the configuration for waterline
    * connection to an online PostgreSQL

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-var express = require('express');
+const app = require('express')();
+const orm = new require('waterline')();
+const bodyParser = require('body-parser');
+const methodOverRide = require('method-override');
 
-var app = express();
+const DBconfig = require('./config/waterlineConfig').DBconfig;
+
+// orm.loadCollection(Message);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverRide());
 
 app.get('/', (req, res) => {
     console.log("get on /");
-    res.send("hello world!");
+    res.send("go to see things on /messages");
 });
 
-app.listen(3000, () => {
+
+orm.initialize(DBconfig, (err, models) => {
+  if (err) throw err;
+
+  app.models = models.collections;
+  app.connections = models.connections;
+
+  app.listen(3000, () => {
     console.log("listen on port 3000");
+  });
 });
