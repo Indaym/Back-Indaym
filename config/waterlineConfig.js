9b89c1f16354de46fc3d6fb627698854fceef938
Nicolas Delahaigue <nicolas.delahaigue@epitech.eu>
2017-10-05 16:10:00 +0200

    Fix multidatabase issue (#27)
    * Add flag --config to choose witch db config use (#27)

--- config/waterlineConfig.js
+++ config/waterlineConfig.js
@@ -4,31 +4,14 @@
 
 const PGAdapter = require('waterline-postgresql');
 
-module.exports.DBconfig = {
-  adapters: {
-    'postgres': PGAdapter,
-  },
-
+/*
+ * Default config used for everyone
+ */
+const defaultConfig = {
+  adapters: {},
   connections: {
     postgresdb: {
       adapter: 'postgres',
-      connection: {
-        database: 'bnc4cnogpb2rxwd',
-        host: 'bnc4cnogpb2rxwd-postgresql.services.clever-cloud.com',
-        user: 'uy0rfk0tyaulc9garili',
-        password: 'IdTezRfmU1Onx6NhynUf',
-        port: 5432,
-        ssl: false,
-      },
-      /*
-      connection: {
-        database: 'main',
-        host: 'localhost',
-        user: 'indaym',
-        password: 'indaym',
-        port: '4001',
-        ssl: false,
-      },*/
       pool: {
         min: 2,
         max: 20,
@@ -37,14 +20,53 @@ module.exports.DBconfig = {
   },
 };
 
-/**
-      connection: {
-        database: 'bnc4cnogpb2rxwd',
-        host: 'bnc4cnogpb2rxwd-postgresql.services.clever-cloud.com',
-        user: 'uy0rfk0tyaulc9garili',
-        password: 'IdTezRfmU1Onx6NhynUf',
-        port: 5432,
-        ssl: false,
-      },
-
+/*
+ * Stringify of default config to be parse after
  */
+const defaultConfigStr = JSON.stringify(defaultConfig);
+
+/*
+ * Setup environment 
+ * 
+ * - Parse stringified defaulConfig to create new object
+ * - Set PGAdapter need reference can't be stringified
+ * - Set custom connection
+ */
+function setup(connection) {
+  const env = JSON.parse(defaultConfigStr); // Really dirty but need deep copy, any suggestion ?
+  env.adapters['postgres'] = PGAdapter;
+  env.connections.postgresdb.connection = connection;
+  return env;
+}
+
+/*
+ * Environments
+ */
+
+// Production
+const production = setup({
+  database: 'bnc4cnogpb2rxwd',
+  host: 'bnc4cnogpb2rxwd-postgresql.services.clever-cloud.com',
+  user: 'uy0rfk0tyaulc9garili',
+  password: 'IdTezRfmU1Onx6NhynUf',
+  port: 5432,
+  ssl: false,
+});
+
+// Development / Local docker
+const development = setup({
+  database: 'main',
+  host: 'localhost',
+  user: 'indaym',
+  password: 'indaym',
+  port: 4001,
+  ssl: false,
+});
+
+/*
+ * export of environments
+ */
+module.exports = {
+  production,
+  development,
+};
